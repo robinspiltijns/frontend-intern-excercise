e8d75190d3a33e0c5707211d0b1ccf5f3a9f20d2
robinspitlijns <spiltijnsrobin@gmail.com>
2020-07-11 16:42:48 +0200

    remove empty css file

--- src/app/components/project-info-card/project-info-card.component.ts
+++ src/app/components/project-info-card/project-info-card.component.ts
@@ -5,7 +5,6 @@ import { label } from '../../priority';
 @Component({
   selector: 'app-project-info-card',
   templateUrl: './project-info-card.component.html',
-  styleUrls: ['./project-info-card.component.css']
 })
 export class ProjectInfoCardComponent { // Maybe need an Oninit?
   @Input() project: Project;
